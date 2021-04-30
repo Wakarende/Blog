ab15207dc5ccafbd275542db1cb6097079419dc9
Joy Kirii <joykirii@gmail.com>
2021-04-30 21:05:55 +0300

    create view function and html for profile section

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,17 @@
 from flask import render_template,request,redirect,url_for,abort,flash
 from . import main
+from ..models import User
 
 @main.route('/')
 def index():
   title = 'Blog'
   return render_template('index.html', title=title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+  user = User.query.filter_by(username = uname).first()
+
+  if user is None:
+    abort(404)
+
+  return render_template("profile/profile.html", user = user)
