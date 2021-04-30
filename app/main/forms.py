00b1c83efd7b41e208e7b40c19ccab0fd446b80c
Joy Kirii <joykirii@gmail.com>
2021-04-30 21:08:41 +0300

    create update profile form

---
+++ app/main/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,TextAreaField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
+
+class UpdateProfile(FlaskForm):
+  bio = TextAreaField('Tell us about you.',validators = [Required()])
+  submit = SubmitField('Submit')
+
+  
