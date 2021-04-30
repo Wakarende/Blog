3a4d6417dcdba2dd3d1a2aa879b1a1d5dfd76e58
Joy Kirii <joykirii@gmail.com>
2021-04-30 18:27:51 +0300

    create logout user view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,redirect,url_for,flash,request
 from . import auth
-from flask_login import login_required,login_user
+from flask_login import login_required,login_user,logout_user
 from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
@@ -30,3 +30,10 @@ def register():
     return redirect(url_for('auth.login'))
     title = "New Account"
   return render_template('auth/register.html',registration_form = form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for("main.index"))
