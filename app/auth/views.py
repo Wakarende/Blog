935fdf5e653218505dc4eb8566081e80eaf61d31
Joy Kirii <joykirii@gmail.com>
2021-04-30 18:07:16 +0300

    create registration form with custom validators

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,8 @@
 from flask import render_template
-from . import 
+from . import auth
+from flask_login import login_required
 
 @auth.route('/login')
 def login():
   return render_template('auth/login.html')
 
-  
